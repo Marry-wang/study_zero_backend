4cd1acc3d013822ac983ecb30898018a3a668ee0
wangmengwei <1730313139@qq.com>
2023-02-15 17:31:06 +0800

    更新sql文件sys_user/sys_menu

--- sql/zero.sql
+++ sql/zero.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80030
  File Encoding         : 65001
 
- Date: 17/01/2023 17:55:01
+ Date: 15/02/2023 17:29:59
 */
 
 SET NAMES utf8mb4;
@@ -23,13 +23,41 @@ SET FOREIGN_KEY_CHECKS = 0;
 DROP TABLE IF EXISTS `sys_menu`;
 CREATE TABLE `sys_menu`  (
   `id` int(0) NOT NULL AUTO_INCREMENT,
+  `parent_id` int(0) NULL DEFAULT NULL COMMENT '父id',
   `menu_path` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '菜单路径',
   `menu_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '菜单名称',
   `menu_icon` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '菜单图标',
   `create_time` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
+  `create_by` int(0) NULL DEFAULT NULL,
   `update_time` datetime(0) NULL DEFAULT NULL COMMENT '更新时间',
+  `update_by` int(0) NULL DEFAULT NULL,
   `dele_flag` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT '0' COMMENT '是否删除(0false/1true)',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of sys_menu
+-- ----------------------------
+INSERT INTO `sys_menu` VALUES (1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '0');
+INSERT INTO `sys_menu` VALUES (2, 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '0');
+INSERT INTO `sys_menu` VALUES (3, 2, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '0');
+INSERT INTO `sys_menu` VALUES (4, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '0');
+INSERT INTO `sys_menu` VALUES (5, 4, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '0');
+INSERT INTO `sys_menu` VALUES (6, 5, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '0');
+
+-- ----------------------------
+-- Table structure for sys_user
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_user`;
+CREATE TABLE `sys_user`  (
+  `id` int(0) NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `create_time` datetime(0) NULL DEFAULT NULL,
+  `cretae_by` int(0) NULL DEFAULT NULL,
+  `update_time` datetime(0) NULL DEFAULT NULL,
+  `update_by` int(0) NULL DEFAULT NULL,
+  `dele_flag` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 SET FOREIGN_KEY_CHECKS = 1;
