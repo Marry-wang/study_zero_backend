6e81af9cdf8f77b22dee0b3157675f66e6d1b6a1
wangmengwei <1730313139@qq.com>
2023-03-14 15:05:41 +0800

    feat:gateway植入并访问user接口

---
+++ studay_zero_gateway/src/main/java/com/demo/GatewayApplication.java
@@ -0,0 +1,16 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: 王孟伟
+ * @Date: 2023/3/13 14:02
+ * @Description:
+ */
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[]args){
+        SpringApplication.run(GatewayApplication.class,args);
+    }
+}
