087cd7c19b3b5a050dc1c831b9df99af0f6e686c
wangmengwei <1730313139@qq.com>
2023-07-19 23:31:39 +0800

    feat:ExceptionHandler

---
+++ study_zero_module/study_zero_system/src/main/java/com/demo/handler/StudayZeroExceptionHandler.java
@@ -0,0 +1,15 @@
+package com.demo.handler;
+
+import com.demo.api.ZeroResult;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class StudayZeroExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ZeroResult handleException(Exception e) {
+        System.out.printf(e.toString());
+        return ZeroResult.fail(5001,e.getMessage());
+    }
+}
