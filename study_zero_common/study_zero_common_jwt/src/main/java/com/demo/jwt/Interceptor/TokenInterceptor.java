46f9fe10011295484607fe1a67de0efe18c465dd
wangmengwei <1730313139@qq.com>
2023-08-30 16:53:24 +0800

    feat:屏蔽以前的拦截

--- study_zero_common/study_zero_common_jwt/src/main/java/com/demo/jwt/Interceptor/TokenInterceptor.java
+++ study_zero_common/study_zero_common_jwt/src/main/java/com/demo/jwt/Interceptor/TokenInterceptor.java
@@ -23,47 +23,47 @@ public class TokenInterceptor extends HandlerInterceptorAdapter {
     @Autowired
     private JwtConfig jwtConfig;
 
-    @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        String url = request.getRequestURI();
-        if(url.equals("/demo/")){
-            return true;
-        }
-        if(url.contains("/login")){
-            return true;
-        }
-        if(url.contains("/swagger")){
-            return true;
-        }
-        if(url.contains("/error")){
-            return true;
-        }
-        if(url.contains("/csrf")){
-            return true;
-        }
-
-
-        /**
-         * token 验证
-         */
-        String token = request.getHeader(jwtConfig.getHeader());
-        if(StringUtils.isEmpty(token)){
-            token = request.getParameter(jwtConfig.getHeader());
-        }
-        if(StringUtils.isEmpty(token)){
-            throw new SignatureException(jwtConfig.getHeader()+"不能为空");
-        }
-        Claims claims = null;
-        try {
-            claims = jwtConfig.getTokenClaim(token);
-            if(claims == null || jwtConfig.isTokenExpired(claims.getExpiration())){
-                throw new SignatureException(jwtConfig.getHeader()+"失效,请重新登录");
-            }
-        }catch (Exception e){
-            throw new SignatureException(jwtConfig.getHeader()+"失效,请重新登录");
-        }
-        request.setAttribute("identityId",claims.getSubject());
-        return true;
-    }
+//    @Override
+//    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+//        String url = request.getRequestURI();
+//        if(url.equals("/demo/")){
+//            return true;
+//        }
+//        if(url.contains("/login")){
+//            return true;
+//        }
+//        if(url.contains("/swagger")){
+//            return true;
+//        }
+//        if(url.contains("/error")){
+//            return true;
+//        }
+//        if(url.contains("/csrf")){
+//            return true;
+//        }
+//
+//
+//        /**
+//         * token 验证
+//         */
+//        String token = request.getHeader(jwtConfig.getHeader());
+//        if(StringUtils.isEmpty(token)){
+//            token = request.getParameter(jwtConfig.getHeader());
+//        }
+//        if(StringUtils.isEmpty(token)){
+//            throw new SignatureException(jwtConfig.getHeader()+"不能为空");
+//        }
+//        Claims claims = null;
+//        try {
+//            claims = jwtConfig.getTokenClaim(token);
+//            if(claims == null || jwtConfig.isTokenExpired(claims.getExpiration())){
+//                throw new SignatureException(jwtConfig.getHeader()+"失效,请重新登录");
+//            }
+//        }catch (Exception e){
+//            throw new SignatureException(jwtConfig.getHeader()+"失效,请重新登录");
+//        }
+//        request.setAttribute("identityId",claims.getSubject());
+//        return true;
+//    }
 
 }
--- study_zero_common/study_zero_common_jwt/src/main/java/com/demo/jwt/jwtconfig/JwtConfig.java
+++ study_zero_common/study_zero_common_jwt/src/main/java/com/demo/jwt/jwtconfig/JwtConfig.java
@@ -22,20 +22,20 @@ import java.util.Date;
  * https://blog.csdn.net/AkiraNicky/article/details/99307713?ops_request_misc=%257B%2522request%255Fid%2522%253A%2522163549409116780265479171%2522%252C%2522scm%2522%253A%252220140713.130102334..%2522%257D&request_id=163549409116780265479171&biz_id=0&utm_medium=distribute.pc_search_result.none-task-blog-2~all~top_positive~default-1-99307713.first_rank_v2_pc_rank_v29&utm_term=springBoot%E6%95%B4%E5%90%88jwt&spm=1018.2226.3001.4187
  */
 @Component
-//@ConfigurationProperties(prefix = "config.jwt")
+@ConfigurationProperties(prefix = "config.jwt")
 @Data
 public class JwtConfig {
 
     /**
      * @ConstructorProperties
      */
-    @Value("${config.jwt.secret}")
+//    @Value("${config.jwt.secret}")
     private String secret;
 
-    @Value("${config.jwt.expire}")
+//    @Value("${config.jwt.expire}")
     private long  expire;
 
-    @Value("${config.jwt.header}")
+//    @Value("${config.jwt.header}")
     private String header;
 
     /**
