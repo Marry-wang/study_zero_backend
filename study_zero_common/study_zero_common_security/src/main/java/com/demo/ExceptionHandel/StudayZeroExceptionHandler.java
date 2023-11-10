222a576168a13c586dd6efc90c9571dca432ac19
wangmengwei <1730313139@qq.com>
2023-11-10 13:43:42 +0800

    fix:1.全局异常只需要在异常统一处理中，进行异常抛出，在gateway中无法进行 2.每一个项目的日志处理，配置输出到哪一个包下，root目录打印项目全部

--- study_zero_common/study_zero_common_security/src/main/java/com/demo/ExceptionHandel/StudayZeroExceptionHandler.java
+++ study_zero_common/study_zero_common_security/src/main/java/com/demo/ExceptionHandel/StudayZeroExceptionHandler.java
@@ -2,9 +2,10 @@ package com.demo.ExceptionHandel;
 
 import com.demo.api.ZeroResult;
 import com.demo.exception.BaseException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
+@Slf4j
 @RestControllerAdvice
 public class StudayZeroExceptionHandler {
 
@@ -12,5 +13,12 @@ public class StudayZeroExceptionHandler {
     public ZeroResult handleException(BaseException e) {
         System.out.printf(e.toString());
         return ZeroResult.fail(e.getErrorCode(), e.getMessage());
+    }
+
+
+    @ExceptionHandler(Exception.class)
+    public ZeroResult handleException(Exception e) {
+        log.error(e.getMessage(),e);
+        return ZeroResult.fail(5001,e.getMessage());
     }
 }
