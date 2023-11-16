59358a79d3090bd2c5c9526c17d9326611ceef41
wangmengwei <1730313139@qq.com>
2023-11-16 14:34:37 +0800

    fix:将配置信息送到nacos

--- study_zero_module/study_zero_system/src/main/java/com/demo/SystemApplication.java
+++ study_zero_module/study_zero_system/src/main/java/com/demo/SystemApplication.java
@@ -2,11 +2,13 @@ package com.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author wangmengwei
  * @date 2022年06月08日 17:10
  */
+@EnableDiscoveryClient
 @SpringBootApplication
 public class SystemApplication {
     public static void main(String[] args) {
