79e0950bc22f6b7c7ada8288eeeadf7f1a342892
wangmengwei <1730313139@qq.com>
2023-11-17 10:05:06 +0800

    fix:gateway配置修改为nacos服务配置

--- study_zero_gateway/src/main/java/com/demo/GatewayApplication.java
+++ study_zero_gateway/src/main/java/com/demo/GatewayApplication.java
@@ -2,12 +2,14 @@ package com.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @Author: 王孟伟
  * @Date: 2023/3/13 14:02
  * @Description:
  */
+@EnableDiscoveryClient
 @SpringBootApplication
 public class GatewayApplication {
     public static void main(String[] args) {
