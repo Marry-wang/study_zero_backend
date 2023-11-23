aabb0ba8c9cbf49583efd5512d438cc53c284f98
wangmengwei <1730313139@qq.com>
2023-11-23 17:24:24 +0800

    fix:sql文件更新

--- sql/zero.sql
+++ sql/zero.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80030
  File Encoding         : 65001
 
- Date: 28/07/2023 15:26:39
+ Date: 23/11/2023 17:23:41
 */
 
 SET NAMES utf8mb4;
@@ -33,7 +33,7 @@ CREATE TABLE `sys_menu`  (
   `update_by` int(0) NULL DEFAULT NULL,
   `dele_flag` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT '0' COMMENT '是否删除(0false/1true)',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 12 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 15 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of sys_menu
@@ -61,7 +61,7 @@ CREATE TABLE `sys_role`  (
   `update_by` int(0) NULL DEFAULT NULL,
   `del_flag` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT '0',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 9 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of sys_role
@@ -69,7 +69,6 @@ CREATE TABLE `sys_role`  (
 INSERT INTO `sys_role` VALUES (2, 'www', NULL, NULL, NULL, NULL, '0');
 INSERT INTO `sys_role` VALUES (3, 'aaaaa', NULL, NULL, NULL, NULL, '0');
 INSERT INTO `sys_role` VALUES (4, 'aaa', NULL, NULL, NULL, NULL, '0');
-INSERT INTO `sys_role` VALUES (5, 'qq', NULL, NULL, NULL, NULL, '0');
 
 -- ----------------------------
 -- Table structure for sys_role_menu
@@ -88,10 +87,24 @@ CREATE TABLE `sys_role_menu`  (
 -- ----------------------------
 -- Records of sys_role_menu
 -- ----------------------------
-INSERT INTO `sys_role_menu` VALUES (2, 3, NULL, NULL, NULL, NULL, '0');
-INSERT INTO `sys_role_menu` VALUES (2, 5, NULL, NULL, NULL, NULL, '0');
 INSERT INTO `sys_role_menu` VALUES (3, 3, NULL, NULL, NULL, NULL, '0');
 INSERT INTO `sys_role_menu` VALUES (3, 5, NULL, NULL, NULL, NULL, '0');
+INSERT INTO `sys_role_menu` VALUES (2, 1, NULL, NULL, NULL, NULL, '0');
+INSERT INTO `sys_role_menu` VALUES (2, 8, NULL, NULL, NULL, NULL, '0');
+INSERT INTO `sys_role_menu` VALUES (2, 9, NULL, NULL, NULL, NULL, '0');
+INSERT INTO `sys_role_menu` VALUES (2, 10, NULL, NULL, NULL, NULL, '0');
+INSERT INTO `sys_role_menu` VALUES (2, 11, NULL, NULL, NULL, NULL, '0');
+INSERT INTO `sys_role_menu` VALUES (2, 4, NULL, NULL, NULL, NULL, '0');
+INSERT INTO `sys_role_menu` VALUES (2, 2, NULL, NULL, NULL, NULL, '0');
+INSERT INTO `sys_role_menu` VALUES (2, 3, NULL, NULL, NULL, NULL, '0');
+INSERT INTO `sys_role_menu` VALUES (2, 5, NULL, NULL, NULL, NULL, '0');
+INSERT INTO `sys_role_menu` VALUES (8, 2, NULL, NULL, NULL, NULL, '0');
+INSERT INTO `sys_role_menu` VALUES (8, 3, NULL, NULL, NULL, NULL, '0');
+INSERT INTO `sys_role_menu` VALUES (4, 1, NULL, NULL, NULL, NULL, '0');
+INSERT INTO `sys_role_menu` VALUES (4, 8, NULL, NULL, NULL, NULL, '0');
+INSERT INTO `sys_role_menu` VALUES (4, 9, NULL, NULL, NULL, NULL, '0');
+INSERT INTO `sys_role_menu` VALUES (4, 10, NULL, NULL, NULL, NULL, '0');
+INSERT INTO `sys_role_menu` VALUES (4, 11, NULL, NULL, NULL, NULL, '0');
 
 -- ----------------------------
 -- Table structure for sys_user
@@ -106,13 +119,13 @@ CREATE TABLE `sys_user`  (
   `update_by` int(0) NULL DEFAULT NULL,
   `dele_flag` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT '0',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 7 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of sys_user
 -- ----------------------------
 INSERT INTO `sys_user` VALUES (1, 'admin', NULL, NULL, NULL, NULL, '0');
-INSERT INTO `sys_user` VALUES (3, 'adminOne1', NULL, NULL, NULL, NULL, '0');
+INSERT INTO `sys_user` VALUES (6, 'marry', NULL, NULL, NULL, NULL, '0');
 
 -- ----------------------------
 -- Table structure for sys_user_role
@@ -134,5 +147,6 @@ CREATE TABLE `sys_user_role`  (
 INSERT INTO `sys_user_role` VALUES (1, 3, NULL, NULL, NULL, NULL, '0');
 INSERT INTO `sys_user_role` VALUES (1, 4, NULL, NULL, NULL, NULL, '0');
 INSERT INTO `sys_user_role` VALUES (1, 2, NULL, NULL, NULL, NULL, '0');
+INSERT INTO `sys_user_role` VALUES (6, 4, NULL, NULL, NULL, NULL, '0');
 
 SET FOREIGN_KEY_CHECKS = 1;
