be962f85b996c1e4715f403f47ee08e885371a19
wangmengwei <wangmengwei@sdswxx.com>
2023-12-01 15:27:54 +0800

    fix:删除不能作为common的项目

--- study_zero_common/study_zero_common_es/src/main/java/com/demo/backend/mapper/BookeMapper.java
+++
@@ -1,9 +0,0 @@
-package com.demo.backend.mapper;
-
-import com.demo.backend.demo.Book;
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-
-public interface BookeMapper extends ElasticsearchRepository<Book,String> {
-
-
-}
